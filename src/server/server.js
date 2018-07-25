a020cc9f14e66bfad92ea7db090fd4b1a6e5702a
Simon Schneider <simon.sch@outlook.com>
2018-07-25 18:04:14 +0200

    TASK Add routes for delivering static files

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require("path")
 const fourSquareApi = require('./services/fourSquareApi')
 const {respondWithError} = require('./services/errorHandling')
 const config = require("./config")
 
 const app = express();
+
+const root = path.join(__dirname, '..', '..')
+
+app.use(express.static(path.join(root, 'build')))
 
 app.get('/api/hello', (req, res) => {
   res.send({express: 'Hello FiveCircle API.'});
@@ -15,6 +20,14 @@ app.get("/api/get-venues", (req, res) => {
       res.json(venuesResponse)
     })
     .catch(error => respondWithError(res, error.message))
+})
+
+app.get('/api/*', (req, res) => {
+  res.send("Unkown API endpoint.")
+})
+
+app.get('/*', (req, res) => {
+  res.sendFile(root + '/build/index.html');
 })
 
 app.listen(config.SERVER.PORT, () => console.log(`FiveCircle API listening on port ${config.SERVER.PORT}`));
